062d934aa65b4a08b4c84d0a706e0e7a091bffcd
umerk24 <100189358+umerk24@users.noreply.github.com>
2023-01-13 06:53:41 +0500

    Add files via upload

---
+++ MemberShip.cpp
@@ -0,0 +1,50 @@
+#pragma once
+#include<iostream>
+#include"MemberShip.h"
+using namespace std;
+
+
+member::member()
+{
+    this->un="";
+    this->pass="";
+}
+void member::setpass(string s)
+{
+    pass=s;
+}
+string member::getpass()
+{
+    return pass;
+}
+void member::setun(string u)
+{
+    un =u;
+}
+string member::getun()
+{
+    return un;
+}
+void member::MemberShips()
+{
+    cout<<"\t\tMEMBERS BENEFIT" <<endl;
+    cout<<"\t\t 1) Discount on drinks"<< endl;
+    cout<<"\t\t 2) 10% off on Tickets"<< endl;
+    cout<<"\t\t 3) Free 3D glasses"<<endl;
+}
+void member::admin()
+{
+    cout<<"\t\t\t\t\t WELCOME TO MOVIE TICKETING SYSTEM" << endl;
+    cout<<"\n ENTER USERNAME : ";
+    cin>>un;
+    setun(un);
+    if(un=="a" || un=="A")
+    {
+        cout<<"\n ENTER PASSWORD : ";
+        cin>>pass;
+        if(pass =="a" || pass=="A")
+        {
+            setpass(pass);
+        }
+    }
+}
